7ad0c7b81e6c5532819402b7e76716f4adb92db9
Erwin Rodrigues <erwin.rodrigues@epita.fr>
2024-01-09 16:20:43 +0100

    File Tree

---
+++ src/lexer.h
@@ -0,0 +1,20 @@
+#ifndef LEXER_H
+#define LEXER_H
+
+enum token
+{
+    TOKEN_IF,
+    TOKEN_THEN,
+    TOKEN_ELIF,
+    TOKEN_ELSE,
+    TOKEN_FI,
+    TOKEN_COLON,
+    TOKEN_NL,
+    TOKEN_SQUOTE,
+    TOKEN_WORDS
+}
+
+func string -> token
+// DO IT ALONE
+
+#endif // LEXER_H
