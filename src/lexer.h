f8dc9a1d1b720fd7d906b966bdf1cba87abecf6c
Erwin Rodrigues <erwin.rodrigues@epita.fr>
2024-01-09 16:39:05 +0100

    Lexer Genesis

--- src/lexer.h
+++ src/lexer.h
@@ -1,7 +1,9 @@
 #ifndef LEXER_H
 #define LEXER_H
 
-enum token
+#include <stddef.h>
+
+enum token_type
 {
     TOKEN_IF,
     TOKEN_THEN,
@@ -12,9 +14,25 @@ enum token
     TOKEN_NL,
     TOKEN_SQUOTE,
     TOKEN_WORDS
-}
+};
 
-func string -> token
-// DO IT ALONE
+struct token
+{
+    enum token_type type;
+    char *value;
+    struct token *next;
+};
+
+struct token_list
+{
+    struct token *head;
+    size_t size;
+};
+
+void token_push(struct token_list tokens, struct token token);
+struct token *token_pop(struct token_list tokens);
+void token_free(struct token token);
+void token_list_destroy(struct token_list tokens);
+struct token_list token_list_genesis(char *input);
 
 #endif // LEXER_H
