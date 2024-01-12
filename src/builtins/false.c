e51c19c438444c9c75b62a829a72d8f170f5a777
Felix Pilewicz <felix.pilewicz@epita.fr>
2024-01-12 13:28:28 +0100

    feat: true and false are now properly documented

--- src/builtins/false.c
+++ src/builtins/false.c
@@ -1,6 +1,10 @@
 
 #include "builtin.h"
 
+/**
+** \brief Returns false in shell (1)
+** \return an int that represents false (1)
+*/
 int bin_false(void)
 {
     return 1;
--- src/builtins/true.c
+++ src/builtins/true.c
@@ -1,5 +1,9 @@
 #include "builtin.h"
 
+/**
+** \brief Returns true in shell (0)
+** \return an int that represents true (0)
+*/
 int bin_true(void)
 {
     return 0;
