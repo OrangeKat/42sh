421f967dacb69a12d11b9db1a0a7a2f2b7f5426b
Felix Pilewicz <felix.pilewicz@epita.fr>
2024-01-19 20:05:57 +0100

    fix: various fixes to build system and revamp of main

--- src/main.c
+++ src/main.c
@@ -1,42 +1,44 @@
 #define _POSIX_C_SOURCE 200809L
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
 #include <err.h>
 #include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-#include "lexer_parser/eval/eval.h"
-#include "utils/file_to_string.h"
-#include "lexer_parser/parser/parser.h"
-#include "lexer_parser/lexer/lexer.h"
-#include "lexer_parser/ast/ast.h"
 #include "builtins/builtin.h"
+#include "lexer_parser/ast/ast.h"
+#include "lexer_parser/eval/eval.h"
+#include "lexer_parser/lexer/lexer.h"
+#include "lexer_parser/parser/parser.h"
+#include "utils/file_to_string.h"
 
-int main(int argc,char **argv)
+int main(int argc, char **argv)
 {
-    if (argc == 0)
-    {
-        return 1;
-    }
     FILE *f = NULL;
-    //check if it there is a string argument
-    if(strcmp("-c",argv[1]) == 0)
-    {
-        //create a file and copy argv[2] inside
-        f = fmemopen(argv[2],strlen(argv[2]),"r");
-    }
     // stdin
-    else if(strcmp("<" ,argv[1]))
+    if (argc == 1)
     {
-        //cherck if file or not 
     }
-    //if file 
+    // check if it there is a string argument
+    else if (strcmp("-c", argv[1]) == 0)
+    {
+        // raise an error "use case: ./42sh -c [command]"
+        if (argc == 2)
+        {
+        }
+        // create a file and copy argv[2] inside
+        f = fmemopen(argv[2], strlen(argv[2]), "r");
+    }
     else
     {
-        if((f = fopen(argv[1],"r")) == NULL)
+        if ((f = fopen(argv[1], "r")) == NULL)
         {
-            err(-1,"not a file");
+            err(-1, "not a file");
+        }
+        // file
+        else
+        {
         }
     }
 
