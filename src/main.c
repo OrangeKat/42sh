2304c6287e7042badb1917823dcea4e02bd30ec3
Felix Pilewicz <felix.pilewicz@epita.fr>
2024-01-20 12:36:49 +0100

    refactor: refactored main.c to better suit coding style

--- src/main.c
+++ src/main.c
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #include "builtins/builtin.h"
 #include "lexer_parser/ast/ast.h"
@@ -13,33 +14,63 @@
 #include "lexer_parser/parser/parser.h"
 #include "utils/file_to_string.h"
 
+int stdin_handler(FILE **f)
+{
+    char buffer[1024];
+    if (read(STDIN_FILENO, buffer, 1024) < 0)
+    {
+        // error handling: could not read stdin
+    }
+    *f = fmemopen(buffer, strlen(buffer), "r");
+    return 0;
+}
+
+int command_line_handler(FILE **f, int argc, char **argv)
+{
+    // raise an error "use case: ./42sh -c [command]"
+    if (argc == 2)
+    {
+    }
+    // create a file and copy argv[2] inside
+    *f = fmemopen(argv[2], strlen(argv[2]), "r");
+    return 0;
+}
+
+int file_handler(FILE **f, char **argv)
+{
+    if ((*f = fopen(argv[1], "r")) == NULL)
+    {
+        err(-1, "not a file");
+    }
+    // file
+    else
+    {
+    }
+    return 0;
+}
+
 int main(int argc, char **argv)
 {
     FILE *f = NULL;
     // stdin
     if (argc == 1)
     {
+        stdin_handler(&f);
     }
     // check if it there is a string argument
     else if (strcmp("-c", argv[1]) == 0)
     {
-        // raise an error "use case: ./42sh -c [command]"
-        if (argc == 2)
-        {
-        }
-        // create a file and copy argv[2] inside
-        f = fmemopen(argv[2], strlen(argv[2]), "r");
+        command_line_handler(&f, argc, argv);
     }
+    // read from a file
     else
     {
-        if ((f = fopen(argv[1], "r")) == NULL)
-        {
-            err(-1, "not a file");
-        }
-        // file
-        else
-        {
-        }
+        file_handler(&f, argv);
+    }
+
+    if (f == NULL)
+    {
+        // error handling: not sure if necessary
     }
 
     struct lexer *lexer = lexer_genesis(f);
