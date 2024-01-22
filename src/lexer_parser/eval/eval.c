e1d41da1b01e5e35419e8b63fbc28efbb9a7522f
Thomas Polo <thomas.polo@epita.fr>
2024-01-22 17:55:51 +0100

    feat: eval handles if_else

--- src/lexer_parser/eval/eval.c
+++ src/lexer_parser/eval/eval.c
@@ -2,7 +2,27 @@
 
 #include "../../builtins/builtin.h"
 
-void ast_eval(struct ast *ast)
+static cmd_handler(char **data, size_t size)
+{
+    if (strcmp(data[0], "true"))
+    {
+        return bin_true();
+    }
+    else if (strcmp(data[0], "false"))
+    {
+        return bin_false();
+    }
+    else if (strcmp(data[0], "echo"))
+    {
+        return !echo(data, size, 0, 1);
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int ast_eval(struct ast *ast)
 {
     if (ast->type == AST_CMD)
     {
@@ -11,13 +31,29 @@ void ast_eval(struct ast *ast)
         {
             size++;
         }
-        echo(ast->data, size, 0, 1);
+        return cmd_handler(ast->data, size);
     }
     else if (ast->type == AST_LIST)
     {
+        int ret_val = 1;
         for (size_t i = 0; i < ast->nb_children; i++)
         {
-            ast_eval(ast->children[i]);
+            if (!ast_eval(ast->children[i]))
+            {
+                ret_val = 0;
+            }
+        }
+        return ret_val;
+    }
+    else if (ast->type == AST_IF)
+    {
+        if (ast_eval(ast->children[0]))
+        {
+            return ast_eval(ast->children[1]);
+        }
+        else
+        {
+            return ast_eval(ast->children[2]);
         }
     }
 }
