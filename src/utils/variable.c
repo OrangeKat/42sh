d14d8ee4608e610d66d311e81214d4a5d014a87a
Felix Pilewicz <felix.pilewicz@epita.fr>
2024-01-24 16:25:03 +0100

    refactored: variables now use a struct that can hold ints, strings or string lists

--- src/utils/variable.c
+++ src/utils/variable.c
@@ -14,82 +14,95 @@
 #define SIFS 2
 #define NMB_ENV_VARS 3
 
+struct var init_var(char *data, enum var_type type, char *name)
+{
+    struct var res = { .type = type, .name = name };
+    if (type == INT)
+    {
+        res.data.numb = atoi(data);
+    }
+    else if (type == STRING)
+    {
+        res.data.string = data;
+    }
+    else
+    {
+        // later
+    }
+    return res;
+}
+
+struct var set_var(struct var var, char *value)
+{
+    if (var.type == INT)
+    {
+        var.data.numb = atoi(value);
+    }
+    else if (var.type == STRING)
+    {
+        var.data.string = value;
+    }
+    else
+    {
+        // later
+    }
+    return var;
+}
+
 struct var_holder *init_var_holder(void)
 {
     struct var_holder *vh = malloc(sizeof(struct var_holder));
-    vh->uvar_names = calloc(1, sizeof(char *));
-    vh->uvar_values = calloc(1, sizeof(char *));
-    vh->spec_values = malloc(sizeof(char *) * NMB_SPECIAL_VARS);
-    vh->arg_values = NULL;
-    vh->env_values = malloc(sizeof(char *) * NMB_ENV_VARS);
+    vh->user_variables = NULL;
+    vh->size = 0;
+    vh->arg_variables = NULL;
     return vh;
 }
 
 void destroy_holder(struct var_holder *vh)
 {
-    size_t i = 0;
-    while (vh->uvar_names[i] != NULL)
-    {
-        free(vh->uvar_names[i]);
-        i++;
-    }
-    free(vh->uvar_names[i]);
-    free(vh->uvar_names);
-
-    i = 0;
-    while (vh->uvar_values[i] != NULL)
-    {
-        free(vh->uvar_values[i]);
-        i++;
-    }
-    free(vh->uvar_values[i]);
-    free(vh->uvar_values);
-
-    for (size_t j = 0; j < NMB_SPECIAL_VARS; j++)
-    {
-        free(vh->spec_values[j]);
-    }
-    free(vh->spec_values);
-
-    for (size_t k = 0; k < NMB_SPECIAL_VARS; k++)
-    {
-        free(vh->env_values[k]);
-    }
-    free(vh->env_values);
+    free(vh->user_variables);
+    free(vh->arg_variables);
+    free(vh);
 }
 
 size_t access_variable(char *name, struct var_holder *vh)
 {
     size_t i = 0;
-    while (vh->uvar_names[i] != NULL)
+    while (i < vh->size)
     {
-        if (strcmp(name, vh->uvar_names[i]) == 0)
+        if (strcmp(name, vh->user_variables[i].name) == 0)
         {
             return i;
         }
+        i++;
     }
     return i;
 }
 
-int set_variable(char *name, char *value, struct var_holder *vh)
+int set_variable(char *name, char *value, enum var_type type,
+                 struct var_holder *vh)
 {
     size_t index = access_variable(name, vh);
-    if (vh->uvar_names[index] == NULL)
+    if (index == vh->size)
     {
-        vh->uvar_names = realloc(vh->uvar_names, (index + 2) * sizeof(char *));
-        vh->uvar_names[index] = name;
-        vh->uvar_names[index + 1] = NULL;
+        vh->size++;
+        vh->user_variables =
+            realloc(vh->user_variables, vh->size * sizeof(struct var));
+        vh->user_variables[index] = init_var(value, type, name);
     }
-    vh->uvar_values[index] = value;
+    else
+    {
+        vh->user_variables[index] = set_var(vh->user_variables[index], value);
+    }
     return 0;
 }
 
-char *get_variable(char *name, struct var_holder *vh)
+struct var get_variable(char *name, struct var_holder *vh)
 {
     size_t index = access_variable(name, vh);
-    if (vh->uvar_names[index] == NULL)
+    if (index == vh->size)
     {
         // error handling
     }
-    return vh->uvar_values[index];
+    return vh->user_variables[index];
 }
--- src/utils/variable.h
+++ src/utils/variable.h
@@ -1,21 +1,48 @@
 #ifndef VARIABLE_H
 #define VARIABLE_H
 
+#include <stddef.h>
+
+enum var_type
+{
+    INT = 0,
+    STRING,
+    LIST,
+    DELIM,
+};
+
+union var_value
+{
+    int numb;
+    char *string;
+    char **list;
+};
+
+struct var
+{
+    enum var_type type;
+    union var_value data;
+    char *name;
+};
+
 struct var_holder
 {
-    char **uvar_names;
-    char **uvar_values;
-    char **spec_values;
-    char **arg_values;
-    char **env_values;
+    struct var *user_variables;
+    size_t size;
+    struct var spec_variables[4];
+    struct var *arg_variables;
+    struct var env_variables[4];
 };
+
+struct var init_var(char *data, enum var_type type, char *name);
 
 struct var_holder *init_var_holder(void);
 
 void destroy_holder(struct var_holder *vh);
 
-int set_variable(char *name, char *value, struct var_holder *vh);
+int set_variable(char *name, char *value, enum var_type type,
+                 struct var_holder *vh);
 
-char *get_variable(char *name, struct var_holder *vh);
+struct var get_variable(char *name, struct var_holder *vh);
 
 #endif /* VARIABLE_H */
