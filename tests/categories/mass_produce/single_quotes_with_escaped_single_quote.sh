a28c91b4f53cbc681b4b41693ee90618ba54bc10
Erwin Rodrigues <erwin.rodrigues@epita.fr>
2024-02-02 20:01:38 +0100

    fix: fixed tests

--- tests/categories/mass_produce/single_quotes_with_escaped_single_quote.sh
+++ tests/categories/mass_produce/single_quotes_with_escaped_single_quote.sh
@@ -7,7 +7,7 @@ TEST_ERR=".teststd.err"
 REF="bash --posix"
 TEST="./../src/42sh"
 
-COMMAND=echo "'This is a test with\'escaped\' single quote.'"
+COMMAND="echo \"'This is a test with\'escaped\' single quote.'\""
 echo $COMMAND >in
 
 cat in | $REF >"$REF_OUT" 2>"$REF_ERR"
